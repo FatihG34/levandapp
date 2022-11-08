a80dbceb497069408d6e92b15213bbf74d2642bc
FatihG34 <fthgnydn34@gmail.com>
2022-11-08 23:38:46 +0300

    carousel modified

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,53 +10,58 @@ import CamFıstıgı from '../assets/cam_fıstıgı.png';
 const Home = () => {
     return (
         <div>
-
-            <div id="animation-carousel" class="relative" data-carousel="static">
-                {/* <!-- Carousel wrapper --> */}
-                <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-                    {/* <!-- Item 1 --> */}
-                    <div class="hidden duration-200 ease-linear absolute inset-0 transition-all transform" data-carousel-item="">
-                        <img src={Hurma} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-                    {/* <!-- Item 2 --> */}
-                    <div class="duration-200 ease-linear absolute inset-0 transition-all transform -translate-x-full z-10" data-carousel-item="">
-                        <img src={Ceviz} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-                    {/* <!-- Item 3 --> */}
-                    <div class="duration-200 ease-linear absolute inset-0 transition-all transform translate-x-0 z-20" data-carousel-item="active">
-                        <img src={Badem} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-                    {/* <!-- Item 4 --> */}
-                    <div class="duration-200 ease-linear absolute inset-0 transition-all transform translate-x-full z-10" data-carousel-item="">
-                        <img src={Kaju} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-                    {/* <!-- Item 5 --> */}
-                    <div class="hidden duration-200 ease-linear absolute inset-0 transition-all transform" data-carousel-item="">
-                        <img src={BrazilCeviz} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-                    <div class="hidden duration-200 ease-linear absolute inset-0 transition-all transform" data-carousel-item="">
-                        <img src={Aycekirdegi} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-                    <div class="hidden duration-200 ease-linear absolute inset-0 transition-all transform" data-carousel-item="">
-                        <img src={CamFıstıgı} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src={Hurma} className="w-2/12 mx-auto" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide5" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
                     </div>
                 </div>
-                {/* <!-- Slider controls --> */}
-                <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev="">
-                    <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
-                        <span class="sr-only">Previous</span>
-                    </span>
-                </button>
-                <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next="">
-                    <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
-                        <span class="sr-only">Next</span>
-                    </span>
-                </button>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src={Ceviz} className="w-2/12 mx-auto" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src={Badem} className="w-2/12 mx-auto" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img src={Kaju} className="w-2/12 mx-auto" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide5" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide5" className="carousel-item relative w-full">
+                    <img src={BrazilCeviz} className="w-2/12 mx-auto" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide6" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide6" className="carousel-item relative w-full">
+                    <img src={Aycekirdegi} className="w-2/12 mx-auto" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide5" className="btn btn-circle">❮</a>
+                        <a href="#slide7" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide7" className="carousel-item relative w-full">
+                    <img src={CamFıstıgı} className="w-2/12 mx-auto" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide6" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
             </div>
-
-            <div className="drawer drawer-end drawer-mobile">
+            <div className="drawer drawer-end drawer-mobile z-10">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
