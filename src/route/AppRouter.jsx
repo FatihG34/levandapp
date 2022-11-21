85b6eb21c7775e3d1b89ece8723a741c68f37169
FatihG34 <fthgnydn34@gmail.com>
2022-11-21 19:46:32 +0300

    modified

--- src/route/AppRouter.jsx
+++ src/route/AppRouter.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from '../components/footer/Footer';
 import Navbar from '../components/navbar/Navbar';
 import AboutUs from '../pages/AboutUs';
 import ContactUs from '../pages/ContactUs';
@@ -14,6 +15,7 @@ const AppRouter = () => {
                 <Route path='/about' element={<AboutUs />} />
                 <Route path='/contact' element={<ContactUs />} />
             </Routes>
+            <Footer />
         </BrowserRouter>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,6 @@ module.exports = {
     logs: true,
     rtl: false,
     prefix: "",
-    darkTheme: "light",
+    darkTheme: false,
   },
 }
