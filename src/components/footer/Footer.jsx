be042153749c1e2b3e5f4dc33d333de6b2d3cfeb
FatihG34 <fthgnydn34@gmail.com>
2022-11-26 13:40:55 +0300

    footer modified

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -41,6 +41,7 @@ const Footer = () => {
                     <a className="link link-hover"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
                 </div>
             </footer>
+
             <footer className="footer footer-center p-10 bg-lime-700 text-primary-content">
                 <div>
                     <img src={Levand} alt="LevandGıda" width={80} />
@@ -56,6 +57,7 @@ const Footer = () => {
                     </div>
                 </div>
             </footer>
+
             <footer className="footer px-5 py-1 border-t bg-base-200 text-base-content border-base-300">
                 <div className="items-center grid-flow-col">
                     <p className='text-xs'>Reliable Tech - Fatih GÜNAYDIN - Copyright © 2022 - All right reserved</p>
