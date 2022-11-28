4b85e80703f664982785305d395d148a79abf8f9
FatihG34 <fthgnydn34@gmail.com>
2022-11-28 17:47:36 +0300

    modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", './src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js',],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{html,js}',
+    './node_modules/tw-elements/dist/js/**/*.js',
+  ],
   theme: {
     extend: {},
     screens: {
