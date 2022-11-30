64065bea205d987d24379a5ecbec0120de51d4de
FatihG34 <fthgnydn34@gmail.com>
2022-11-30 11:50:29 +0300

    modifed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,66 +13,70 @@ import { TfiAngleDoubleUp, TfiArrowCircleLeft } from "react-icons/tfi";
 
 
 
-
-// const sildes = [Hurma, Ceviz, Badem, Kaju, BrazilCeviz, Aycekirdegi, CamFıstıgı]
-const nutsInfo = [
-    {
-        nutName: "Hurma",
-        nutImg: Hurma,
-        nutInfo: "Hurma, Mikroplarla Savaşır İçeriğindeki A ve C vitamini sayesinde vücudun zararlı bakterilerden ve mikroplardan temizlenmesine yardımcı olan güçlü bir antioksidan.Yüksek besin değerinin yanında, bağışıklık sistemini güçlendiren selenyumdan da zengin olması antioksidan etkisini artırır.",
-    },
-    {
-        nutName: "Ceviz",
-        nutImg: Ceviz,
-        nutInfo: "Ceviz sağlıklı yağlar, lifler, vitaminler ve mineraller içerir. Cevizler, diğer yaygın sert kabuklu kuru yemişlerden, daha yüksek oranda antioksidan aktivitesine sahiptir. İç cevizin etrafını saran zarda, E vitamini, melatonin ve polifenol adı verilen bileşikler antioksidan özelliği sağlar.",
-    },
-    {
-        nutName: "Badem",
-        nutImg: Badem,
-        nutInfo: "Badem son derece yüksek besin ihtiva eder. E vitamini, kalsiyum, fosfor, demir ve magnezyum açısından zengin bir kaynaktır. Ayrıca, çinko, selenyum, bakır ve niasin içerir. Diğer bütün türleriyle karşılaştırıldığında besin değerleri ve faydalı bileşenleri ile ön plana çıkmaktadır.",
-    },
-    {
-        nutName: "Kaju",
-        nutImg: Kaju,
-        nutInfo: "Lezzetli yapısıyla  en çok tercih edilen Antik ürünleri arasında arasında yer alır. Çinko içerdiğinden gribe karşı koruyucu etkisi vardır, bağışıklık sistemini güçlendirir, büyüme ve gelişmeyi olumlu etkiler.Her gün tüketilmesi gereken atıştırmalıklardan biridir. İçerdiği minerallerden magnezyum (276 mg/100 gr) kemik ve sinir dokusunu besler, kasların çalışmasını düzenler, kalp atışlarını düzenler. Kansızlığın önemli rahatsızlıklara yol açtığı hamile ve çocuklarda D vitamini içeriği ile eksikliği giderir, kemikleri ve bağışıklık sistemini güçlendirir.",
-    },
-    {
-        nutName: "Brezilya Cevizi",
-        nutImg: BrezilyaCevizi,
-        nutInfo: "Selenyum minerali bakımından en zengin besinlerden biridir.  Brezilya cevizleri bu nedenle atıştırmalık olarak değil 'süper besin' olarak tüketilmektedir. Healthline'da yer alan bilgiye göre günlük belirli miktarda tüketilen Brezilya cevizi kalp, beyin ve bağışıklık sistemini desteklemeye yardımcı olur. Brezilya cevizinin faydaları nedir diye soracak olursak en başta selenyum minerali bakımında zengin olduğunu görürüz.",
-    },
-    {
-        nutName: "Ayçekirdeği",
-        nutImg: Aycekirdegi,
-        nutInfo: "Selenyum minerali bakımından en zengin besinlerden biridir.  Brezilya cevizleri bu nedenle atıştırmalık olarak değil 'süper besin' olarak tüketilmektedir. Healthline'da yer alan bilgiye göre günlük belirli miktarda tüketilen Brezilya cevizi kalp, beyin ve bağışıklık sistemini desteklemeye yardımcı olur. Brezilya cevizinin faydaları nedir diye soracak olursak en başta selenyum minerali bakımında zengin olduğunu görürüz.",
-    },
-    {
-        nutName: "Çam Fıstığı",
-        nutImg: CamFıstıgı,
-        nutInfo: "Çam fıstığı, yağ asidi içeriği sayesinde iştahı bastırır ve kilo vermeye yardımcı olur. Çam fıstığındaki besin maddelerinin mükemmel kombinasyonu enerjiyi artırırken, magnezyum ve protein gibi diğer önemli mineraller kalp krizlerini ve şeker hastalığını önlemeye yardımcı olur.",
-    },
-]
-
 const Home = () => {
     const [silderOpener, setSilderOpener] = useState(false);
     const { setTheme } = useContext(AppContextProv);
     const { t } = useTranslation()
+    const nutsInfo = [
+        {
+            nutName: t("Hurma"),
+            nutName2: "Hurma",
+            nutImg: Hurma,
+            nutInfo: t('nutInfo_hurma'),
+        },
+        {
+            nutName: t("Ceviz"),
+            nutName2: "Ceviz",
+            nutImg: Ceviz,
+            nutInfo: t('nutInfo_ceviz'),
+        },
+        {
+            nutName: t("Badem"),
+            nutName2: "Badem",
+            nutImg: Badem,
+            nutInfo: t('nutInfo_badem'),
+        },
+        {
+            nutName: t("Kaju"),
+            nutName2: "Kaju",
+            nutImg: Kaju,
+            nutInfo: t('nutInfo_kaju'),
+        },
+        {
+            nutName: t("Brezilya_Cevizi"),
+            nutName2: "Brezilya Cevizi",
+            nutImg: BrezilyaCevizi,
+            nutInfo: t('nutInfo_brezilyaCevizi'),
+        },
+        {
+            nutName: t("Ay_cekirdegi"),
+            nutName2: "Ayçekirdeği",
+            nutImg: Aycekirdegi,
+            nutInfo: t('nutInfo_aycekirdegi'),
+        },
+        {
+            nutName: t("Cam_fıstıgı"),
+            nutName2: "Çam Fıstığı",
+            nutImg: CamFıstıgı,
+            nutInfo: t('nutInfo_camFıstıgı'),
+        },
+    ]
     useEffect(() => {
         setSilderOpener(false)
     }, [])
     return (
         <div id='main'>
-            <TfiArrowCircleLeft title='Products Menu' className={`lg:hidden fixed right-8 btn btn-circle ${!silderOpener && 'rotate-180'}`} onClick={() => setSilderOpener(!silderOpener)} />
-            <div className={silderOpener ? `hidden` : `fixed right-5 top-40 z-10`}>
-                <ul className="flex flex-col items-center p-4 w-30 bg-base-100 text-base-content rounded-full py-3 shadow-lg">
+            <div className={`fixed top-48  ${silderOpener ? 'right-0' : '-right-36'} z-10`}>
+                <ul className="menu bg-slate-200 w-56 p-2 rounded-l-box relative">
+                    <TfiArrowCircleLeft title='Products Menu' className={`absolute -top-12 btn btn-circle ${silderOpener && 'rotate-180'}`} onClick={() => setSilderOpener(!silderOpener)} />
                     {/* <!-- Sidebar content here --> */}
-                    <li title='Hurma'><a href='#Hurma'><img src={Hurma} alt="hurma" width={40} className='transition ease-in-out hover:-translate-1 hover:scale-110' /></a></li>
-                    <li title='Ceviz'><a href='#Ceviz'><img src={Ceviz} alt="Ceviz" width={40} className='transition ease-in-out hover:-translate-y-1 hover:scale-110' /></a></li>
-                    <li title='Badem'><a href='#Badem'><img src={Badem} alt="Badem" width={40} className='transition ease-in-out hover:-translate-y-1 hover:scale-110' /></a></li>
-                    <li title='Kaju'><a href='#Kaju'><img src={Kaju} alt="Kaju" width={40} className='transition ease-in-out hover:-translate-y-1 hover:scale-110' /></a></li>
-                    <li title='Brezilya Cevizi'><a href='#Brezilya Cevizi'><img src={BrezilyaCevizi} alt="BrazilCeviz" width={40} className='transition ease-in-out hover:-translate-y-1 hover:scale-110' /></a></li>
-                    <li title='Ayçekirdeği'><a href='#Ayçekirdeği'><img src={Aycekirdegi} alt="Aycekirdegi" width={40} className='transition ease-in-out hover:-translate-y-1 hover:scale-110' /></a></li>
-                    <li title='Çam Fıstıgı'><a href='#Çam Fıstığı'><img src={CamFıstıgı} alt="CamFıstıgı" width={40} className='transition ease-in-out hover:-translate-y-1 hover:scale-110' /></a></li>
+                    <li onClick={() => setSilderOpener(false)} title='Hurma' className='hover-bordered transition ease-in-out hover:-translate-1 hover:scale-110'><a href='#Hurma'><img src={Hurma} alt="hurma" width={40} /><span className={`${silderOpener ? 'inline' : 'hidden'}`} >Hurma</span></a></li>
+                    <li onClick={() => setSilderOpener(false)} title='Ceviz' className='hover-bordered transition ease-in-out hover:-translate-1 hover:scale-110'><a href='#Ceviz'><img src={Ceviz} alt="Ceviz" width={40} /><span className={`${silderOpener ? 'inline' : 'hidden'}`} >Ceviz</span></a></li>
+                    <li onClick={() => setSilderOpener(false)} title='Badem' className='hover-bordered transition ease-in-out hover:-translate-1 hover:scale-110'><a href='#Badem'><img src={Badem} alt="Badem" width={40} /><span className={`${silderOpener ? 'inline' : 'hidden'}`} >Badem</span></a></li>
+                    <li onClick={() => setSilderOpener(false)} title='Kaju' className='hover-bordered transition ease-in-out hover:-translate-1 hover:scale-110'><a href='#Kaju'><img src={Kaju} alt="Kaju" width={40} /><span className={`${silderOpener ? 'inline' : 'hidden'}`} >Kaju</span></a></li>
+                    <li onClick={() => setSilderOpener(false)} title='Brezilya Cevizi' className='hover-bordered transition ease-in-out hover:-translate-1 hover:scale-110'><a href='#Brezilya Cevizi'><img src={BrezilyaCevizi} alt="BrazilCeviz" width={40} /><span className={`${silderOpener ? 'inline' : 'hidden'}`} >Brezilya Cevizi</span></a></li>
+                    <li onClick={() => setSilderOpener(false)} title='Ayçekirdeği' className='hover-bordered transition ease-in-out hover:-translate-1 hover:scale-110'><a href='#Ayçekirdeği'><img src={Aycekirdegi} alt="Aycekirdegi" width={40} /><span className={`${silderOpener ? 'inline' : 'hidden'}`} >Ayçekirdeği</span></a></li>
+                    <li onClick={() => setSilderOpener(false)} title='Çam Fıstıgı' className='hover-bordered transition ease-in-out hover:-translate-1 hover:scale-110'><a href='#Çam Fıstığı'><img src={CamFıstıgı} alt="CamFıstıgı" width={40} /><span className={`${silderOpener ? 'inline' : 'hidden'}`} >Çam Fıstığı</span></a></li>
                 </ul>
             </div>
 
@@ -92,11 +96,11 @@ const Home = () => {
                 </div>
                 {
                     nutsInfo?.map((item, index) => (
-                        <div key={index} className={`hero min-h-screen ${(index % 2 === 0) ? 'bg-base-800' : 'bg-base-200'}`} id={item.nutName}>
+                        <div key={index} className={`hero min-h-screen ${(index % 2 === 0) ? 'bg-base-800' : 'bg-base-200'}`} id={item.nutName2}>
                             <div className={`hero-content flex-col ${(index % 2 === 0) ? 'lg:flex-row' : 'lg:flex-row-reverse'}`}>
                                 <img src={item.nutImg} alt={item.nutName} className="max-w-sm rounded-lg shadow-2xl" />
                                 <div>
-                                    <h1 className="text-5xl font-bold">{item.nutName} Hakkında</h1>
+                                    <h1 className="text-5xl font-bold">{item.nutName}</h1>
                                     <p className="py-6">{item.nutInfo}</p>
                                     {/* <button className="btn btn-primary">Get Started</button> */}
                                 </div>
