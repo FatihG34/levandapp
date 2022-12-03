6369a660a10a6ffcd98a3c528340bf12e8080306
FatihG34 <fthgnydn34@gmail.com>
2022-12-03 22:11:11 +0300

    worked styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     const navigate = useNavigate();
     const { t } = useTranslation()
     return (
-        <>
+        <div className=''>
             <footer className="footer p-10 bg-base-200 text-base-content">
                 <div>
                     <div>
@@ -73,7 +73,7 @@ const Footer = () => {
                     </div>
                 </div>
             </footer>
-        </>
+        </div>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import BrezilyaCevizi from '../assets/brezilyacevizi.png';
 import Aycekirdegi from '../assets/aycekirdegi.png';
 import CamFıstıgı from '../assets/cam_fıstıgı.png';
 import Levand from '../assets/maint.png';
-import { TfiAngleDoubleUp, TfiArrowCircleLeft } from "react-icons/tfi";
+import { TfiAngleDoubleUp, TfiArrowCircleRight } from "react-icons/tfi";
 
 
 
@@ -65,11 +65,11 @@ const Home = () => {
         setSilderOpener(false)
     }, [])
     return (
-        <div id='main'>
-            <div className={`fixed top-48  ${silderOpener ? 'right-0' : '-right-36'} z-10`}>
+        <div id='main' className='scroll-smooth'>
+            {/* <div className={`fixed top-40  ${silderOpener ? 'right-0' : '-right-36'} z-10`}>
                 <ul className="menu bg-slate-200 w-56 p-2 rounded-l-box relative">
                     <TfiArrowCircleLeft title='Products Menu' className={`absolute -top-12 btn btn-circle ${silderOpener && 'rotate-180'}`} onClick={() => setSilderOpener(!silderOpener)} />
-                    {/* <!-- Sidebar content here --> */}
+                    
                     <li onClick={() => setSilderOpener(false)} title='Hurma' className='hover-bordered transition ease-in-out hover:-translate-1 hover:scale-110'><a href='#Hurma'><img src={Hurma} alt="hurma" width={40} /><span className={`${silderOpener ? 'inline' : 'hidden'}`} >{t('Hurma')}</span></a></li>
                     <li onClick={() => setSilderOpener(false)} title='Ceviz' className='hover-bordered transition ease-in-out hover:-translate-1 hover:scale-110'><a href='#Ceviz'><img src={Ceviz} alt="Ceviz" width={40} /><span className={`${silderOpener ? 'inline' : 'hidden'}`} >{t('Ceviz')}</span></a></li>
                     <li onClick={() => setSilderOpener(false)} title='Badem' className='hover-bordered transition ease-in-out hover:-translate-1 hover:scale-110'><a href='#Badem'><img src={Badem} alt="Badem" width={40} /><span className={`${silderOpener ? 'inline' : 'hidden'}`} >{t('Badem')}</span></a></li>
@@ -78,10 +78,68 @@ const Home = () => {
                     <li onClick={() => setSilderOpener(false)} title='Ayçekirdeği' className='hover-bordered transition ease-in-out hover:-translate-1 hover:scale-110'><a href='#Ayçekirdeği'><img src={Aycekirdegi} alt="Aycekirdegi" width={40} /><span className={`${silderOpener ? 'inline' : 'hidden'}`} >{t("Ay_cekirdegi")}</span></a></li>
                     <li onClick={() => setSilderOpener(false)} title='Çam Fıstıgı' className='hover-bordered transition ease-in-out hover:-translate-1 hover:scale-110'><a href='#Çam Fıstığı'><img src={CamFıstıgı} alt="CamFıstıgı" width={40} /><span className={`${silderOpener ? 'inline' : 'hidden'}`} >{t("Cam_fıstıgı")}</span></a></li>
                 </ul>
+            </div> */}
+            {/* <TfiArrowCircleLeft title='Products Menu' className={`absolute -top-12 btn btn-circle ${silderOpener && 'rotate-180'}`} onClick={() => setSilderOpener(!silderOpener)} /> */}
+            <div className={`fixed right-0 flex flex-col items-center ${silderOpener ? 'w-16' : 'w-40'} h-[calc(100vh-90px)] overflow-hidden text-gray-400 bg-gray-100 rounded-l-lg`}>
+                <div className="flex items-center w-full px-3 mt-3 cursor-pointer" onClick={() => setSilderOpener(!silderOpener)} >
+                    <TfiArrowCircleRight title='Products Menu' className={`${silderOpener && 'rotate-180'}`} />
+                    <span className={`ml-2 text-sm font-bold ${silderOpener && 'hidden'} `}>The App</span>
+                </div>
+                <div className="w-full px-2">
+                    <div className="flex flex-col items-center w-full mt-3 border-t border-gray-700">
+                        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href='#Hurma'>
+                            <img src={Hurma} alt="hurma" width={40} />
+                            <span className={`ml-2 text-sm font-medium ${silderOpener && 'hidden'}`}>{t('Hurma')}</span>
+                        </a>
+                        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+                            <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                            </svg>
+                            <span className="ml-2 text-sm font-medium">Search</span>
+                        </a>
+                        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+                            <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                            </svg>
+                            <span className="ml-2 text-sm font-medium">Insights</span>
+                        </a>
+                        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+                            <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
+                            </svg>
+                            <span className="ml-2 text-sm font-medium">Docs</span>
+                        </a>
+                    </div>
+                    <div className="flex flex-col items-center w-full mt-2 border-t border-gray-700">
+                        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+                            <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                            </svg>
+                            <span className="ml-2 text-sm font-medium">Products</span>
+                        </a>
+                        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+                            <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+                            </svg>
+                            <span className="ml-2 text-sm font-medium">Settings</span>
+                        </a>
+                        <a className="relative flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+                            <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                            </svg>
+                            <span className="ml-2 text-sm font-medium">Messages</span>
+                            <span className="absolute top-0 left-0 w-2 h-2 mt-2 ml-2 bg-indigo-500 rounded-full"></span>
+                        </a>
+                    </div>
+                </div>
+                <a className="flex items-center justify-center w-full h-16 mt-auto bg-gray-200 hover:bg-gray-700 hover:text-gray-300" href="#">
+
+                    <TfiAngleDoubleUp className="w-6 h-6 stroke-current" href="#main" />
+                </a>
             </div>
 
 
-            <div className="flex flex-col items-center justify-center">
+            <div className={`absolute flex flex-col items-center justify-center scroll-smooth ${silderOpener ? 'w-[clac(100vw-4rem)]' : 'w-[clac(100vw-10rem)]'}`}>
                 <div className="hero min-h-screen" style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}>
                     <div className="hero-overlay bg-opacity-60"></div>
                     <div className="hero-content text-center text-neutral-content">
@@ -109,7 +167,7 @@ const Home = () => {
                     ))
                 }
             </div>
-            <a href="#main"><button className='fixed bottom-10 right-8 btn btn-circle btn-outline'><TfiAngleDoubleUp /></button></a>
+            {/* <a href="#main"><button className='fixed bottom-10 right-8 btn btn-circle btn-outline'><TfiAngleDoubleUp /></button></a> */}
         </div>
     )
 }
