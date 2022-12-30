0ddc6ea8ec536725067e05b6bce9e6560a451e8a
FatihG34 <fthgnydn34@gmail.com>
2022-12-30 11:11:22 +0300

    bgColor and items modified

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -1,39 +1,47 @@
+import Cookies from 'js-cookie';
 import React, { useEffect } from 'react'
 import { useTranslation } from 'react-i18next';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const AllProducts = (props) => {
+    const currentLanguageCode = Cookies.get('i18next') || 'tr';
     const navigate = useNavigate()
     const { state } = useLocation()
-    const { t } = useTranslation()
+    // const { t } = useTranslation()
     console.log(state)
     const { products } = state
     console.log(products)
     useEffect(() => {
+        console.log(products.productCategory)
         document.title = products.productCategory + "- Levand Gıda Ltd."
-    }, [products.productCategory])
+    }, [products.productCategory, currentLanguageCode])
 
 
     return (
-        <div className='flex flex-col justify-center items-center gap-5 w-full'>
-            <img src={products.productImage} alt="" className='w-full -mt-20' />
-            <h1>{products.productCategory}</h1>
-            <div className=''>
-                {
-                    products.product.map((item, index) => (
-                        <div
-                            className='flex justify-evenly items-center gap-4'
-                            key={index}
-                            onClick={() => navigate('/product', { state: { product: item }, replace: false })}
-                        >
-                            <div className='w-8/12'>
-                                <h1>{item.nutName}</h1>
-                                <p>{item.nutInfo}</p>
+        <div>
+            <div className='flex flex-col justify-center items-center gap-5 w-full -mt-20 h-screen  bg-center bg-cover bg-no-repeat'
+                style={{ backgroundImage: `url(${products.productImage})` }}
+            >
+                <h1 className='sticky top-24 text-3xl bg-lime-100 w-56 text-center p-3 rounded-xl'>{products.productCategory}</h1>
+            </div>
+            <div className='h-screen bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black'>
+                {/* <h1 className='text-3xl bg-lime-100 w-56 text-center p-3 rounded-xl'>{products.productCategory}</h1> */}
+                <div className='flex flex-wrap justify-evenly items-center gap-4 h-full'>
+
+                    {
+                        products.product.map((item, index) => (
+                            <div
+                                key={index}
+                                className='cursor-pointer bg-center bg-cover bg-no-repeat w-40 h-40 flex justify-center items-end rounded-md outline-2 outline-lime-300 outline-double outline-offset-4'
+                                style={{ backgroundImage: `url(${item.nutImg})` }}
+                                onClick={() => navigate('/product', { state: { product: item }, replace: false })}
+                            >
+                                <h1 className='text-white'>{item.nutName}</h1>
+                                {/* <img className="mask mask-squircle w-96" src={item.nutImg} alt="" /> */}
                             </div>
-                            <img className="mask mask-squircle w-96" src={item.nutImg} alt="" />
-                        </div>
-                    ))
-                }
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
