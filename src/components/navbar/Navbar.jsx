a51bbcc2090bc5f2269af5da675dc6e6130ff388
FatihG34 <fthgnydn34@gmail.com>
2023-01-06 10:24:28 +0300

    language selection buttons redesigned

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -130,7 +130,7 @@ const Navbar = () => {
                                 d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
                             ></path>
                         </svg></label>
-                        <ul tabIndex={0} className="menu dropdown-content shadow bg-base-100 rounded-box w-40 mt-4">
+                        <ul tabIndex={0} className="menu dropdown-content shadow bg-[#D9BF3D] rounded-box w-40 mt-4">
                             {languages?.map(({ code, name, country_code }) => (
                                 <li key={country_code}>
                                     <button
